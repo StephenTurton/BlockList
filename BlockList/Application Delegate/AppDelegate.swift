f1623d0113aaaa5031f7a24d81ade790182ac298
StephenTurton <stephenturton@gmail.com>
2018-11-20 12:42:44 -0500

    Project Setup

---
+++ BlockList/Application Delegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BlockList
+//
+//  Created by Stephen Turton on 11/20/18.
+//  Copyright © 2018 Stephen Turton. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ BlockList/View Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BlockList
+//
+//  Created by Stephen Turton on 11/20/18.
+//  Copyright © 2018 Stephen Turton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
